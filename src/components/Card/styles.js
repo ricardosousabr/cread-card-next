3c517bac41066ec80205d4ef6fd440b83fb5f705
ricardo <ricardo.br.pi@gmail.com>
2023-05-25 22:19:07 -0300

    style: card

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -5,12 +5,24 @@ export const ContanerCard = styled.div`
   flex-direction: column;
   background: #7d7676;
   width: 300px;
-  height: 150px;
+  height: 180px;
+  border-radius: 12px;
+`
+export const BoxItens = styled.div`
+  display: flex;
+  margin: 0 0 6px 15px;
+  flex-direction: column;
+  width: 300px;
+  height: 180px;
 `
 
+export const InformationCard = styled.div`
+  display: flex;
+  margin: 15px 0 15px 0;
+`
 export const NumberCard = styled.p`
   color: white;
-  font-size: 15px;
+  font-size: 20px;
 `
 
 export const CardholderName = styled.p`
@@ -27,8 +39,16 @@ export const CardCvc = styled.p`
   color: white;
   font-size: 15px;
 `
+export const BoxCvcExpiration = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 6px;
+  width: 270px;
+`
 
 export const TitleDate = styled.div`
   color: white;
   font-size: 10px;
+  margin: 5px;
 `
